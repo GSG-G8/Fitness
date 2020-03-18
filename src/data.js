28465e6210fc395d596f390b70286004c1978378
Lina Jamal <linahjamal89@gmail.com>
2020-03-18 10:03:46 +0200

    data array related #2

---
+++ src/data.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    id: 1,
+    name: 'Lunges',
+    description:
+      'Challenging your balance is an essential part of a well-rounded exercise routine. Lunges do just that, promoting functional movement, while also increasing strength in your legs and glutes.',
+  },
+  {
+    id: 2,
+    name: 'Pushups',
+    description:
+      'Drop and give me 20! Pushups are one of the most basic yet effective bodyweight moves you can perform because of the number of muscles that are recruited to perform them.',
+  },
+  {
+    id: 3,
+    name: 'Squats',
+    description:
+      'Squats increase lower body and core strength, as well as flexibility in your lower back and hips. Because they engage some of the largest muscles in the body, they also pack a major punch in terms of calories burned.',
+  },
+];
+export default data;
